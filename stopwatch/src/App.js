641a34b478d1609e81e26825a1c5a546261405ca
Your Name <you@example.com>
2024-03-02 13:18:31 +0530

    stopwatch

---
+++ stopwatch/src/App.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+// import "./styles.css";
+
+export default function App() {
+  const [isRunning, setRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  const formatTime = (secs) => {
+    const mins = Math.floor(secs / 60);
+    const remainingSecs = secs % 60;
+    return `${mins}:${remainingSecs < 10 ? "0" : ""}${remainingSecs}`;
+  };
+
+  const toggleTime = () => {
+    setRunning((prevIsRunning) => !prevIsRunning);
+  };
+
+  const reset = () => {
+    setRunning(false);
+    setElapsedTime(0);
+  };
+
+  useEffect(() => {
+    const ac = new AbortController();
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+      ac.abort();
+    };
+  }, [isRunning]);
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <p>Time:{formatTime(elapsedTime)}</p>
+      <button onClick={toggleTime}>{isRunning ? "Stop" : "Start"}</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
